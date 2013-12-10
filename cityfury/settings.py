cf667e0a6569876ce56a721319a29a434554fc91
Ramsey D'silva <ramseydsilva@gmail.com>
2013-12-10 10:33:26 +0100

    Added local settings file

--- cityfury/settings.py
+++ cityfury/settings.py
@@ -57,15 +57,7 @@ ROOT_URLCONF = 'cityfury.urls'
 WSGI_APPLICATION = 'cityfury.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+from local_settings import *
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
